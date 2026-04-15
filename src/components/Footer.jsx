f615473e6b0cea1917d2c9307e283b87bc67e2a7
Krishna Verma <krishnaias1289@gmail.com>
2026-04-15 06:40:42 +0530

    Minor fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2024 Moodify - A Spotify Clone</p>
+      <p>&copy; 2026 Moodify</p>
       <p>Built with React by First Year Students</p>
     </footer>
   )
